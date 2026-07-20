54765bd8d2816d6735bbd3f15a22c2ededcd0ffb
tromshusky <tromshusky@proton.me>
2026-07-20 21:42:10 +0000

    Enhance rebuild-boot.sh with signing functionality
    Refactor boot rebuild script to include signing process for EFI files.

--- rebuild-boot.sh
+++ rebuild-boot.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
-NEXT_NUM=$(efibootmgr | grep -oP "^Boot\K.{4}(?=..HuskyOS Next)") || { echo please create all boot entries including HuskyOS Next; exit 1; }
-newClosure=$(nix build .#nixosConfigurations.nixos.config.system.build.toplevel --no-link --print-out-paths | sed 's|/nix/store|store|' ) || exit 1;
-newUki=$(nix build .#nixosConfigurations.nixos.config.system.build.uki --no-link --print-out-paths)/nixos.efi || exit 1;
-mkdir -p /nix/var/nix/profiles || true;
-ln -snf ../../../$newClosure /nix/var/nix/profiles/system-next;
-cp $newUki /boot/efi/boot/BOOTX64_NEXT.EFI || exit 1;
-efibootmgr -u -n $NEXT_NUM || exit 1;
+PATH=$PATH:${efibootmgr}/bin:${gnused}/bin:${gnugrep}/bin:${nix}/bin:${sbctl}/bin
+unsigned=/boot/efi/boot/BOOTX64_NEXT-unsigned.EFI
+signed=/boot/efi/boot/BOOTX64_NEXT.EFI
+
+unshare -m /bin/sh << 'EOF'
+
+  sbctl --help >/dev/mull || exit 1;
+  NEXT_NUM=$(efibootmgr | grep -oP "^Boot\K.{4}(?=..HuskyOS Next)") || { echo please create all boot entries including HuskyOS Next; exit 1; }
+
+  echo building new system...
+  newUki=$(nix build .#nixosConfigurations.nixos.config.system.build.uki --no-link --print-out-paths --no-write-lock-file)/nixos.efi || exit 1;
+  cp $newUki $unsigned || exit 1;
+  mount --bind /boot/sbctl /var/lib/sbctl &&
+  sbctl status --json | grep '"installed": true' && {
+    sbctl sign $unsigned;
+  } || true;
+  mv -f $unsigned $signed;
+  efibootmgr -u -n $NEXT_NUM || exit 1;
+
+EOF
